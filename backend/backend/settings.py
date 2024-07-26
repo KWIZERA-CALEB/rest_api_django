097e149b92d5719032d6ebcc64ce8e9c43e4e925
KWIZERA-CALEB <146452204+KWIZERA-CALEB@users.noreply.github.com>
2024-07-26 12:02:58 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,7 +183,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
